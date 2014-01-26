7ac6564d53cae235c192bfec9c91c7b5d821480d
Katherine <katherine.fairygirl@hotmail.com>
2014-01-25 20:31:12 -0600

    Update Circulo.java

--- Tarea1/src/Circulo.java
+++ Tarea1/src/Circulo.java
@@ -1,6 +1,8 @@
-/*@author Katherine Aangélica*/
+/*@author Katherine Angélica*/
 
 public class Circulo {
+    
+    //1.El atributo radio tendrá un valor de -4 negativo.
     
     float radio= -4;
 }
